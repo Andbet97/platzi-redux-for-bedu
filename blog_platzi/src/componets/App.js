94fa390f3d80f21ea6e7a426f13e2c086c316374
Andres Betancurt <andbet050197@gmail.com>
2021-02-19 15:19:51 -0500

    Change code to use hooks

--- blog_platzi/src/componets/App.js
+++ blog_platzi/src/componets/App.js
@@ -1,32 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-// React components documentation
-// https://es.reactjs.org/docs/components-and-props.html
-class App extends React.Component  {
-  constructor() {
-    super();
-    this.state = {
-      usuarios: [
-        {
-          nombre: 'Rodolfo',
-          correo: 'Rodolfo@platzi.com',
-          enlace: 'Rodolfo.com'
-        },
-        {
-          nombre: 'Antonio',
-          correo: 'Antonio@platzi.com',
-          enlace: 'Antonio.com'
-        },
-        {
-          nombre: 'Ricardo',
-          correo: 'Ricardo@platzi.com',
-          enlace: 'Ricardo.com'
-        },
-      ]
-    }
-  }
+// React hooks 
+// https://es.reactjs.org/docs/hooks-intro.html
+const App = () => {
 
-	ponerFilas = () => this.state.usuarios.map((usuario) => (
+  const [usuarios, setUsuarios] = useState([
+    {
+      nombre: 'Rodolfo',
+      correo: 'Rodolfo@platzi.com',
+      enlace: 'Rodolfo.com'
+    },
+    {
+      nombre: 'Antonio',
+      correo: 'Antonio@platzi.com',
+      enlace: 'Antonio.com'
+    },
+    {
+      nombre: 'Ricardo',
+      correo: 'Ricardo@platzi.com',
+      enlace: 'Ricardo.com'
+    },
+  ]);
+
+	const ponerFilas = () => usuarios.map((usuario) => (
 		<tr>
 			<td>
 				{ usuario.nombre }
@@ -40,30 +36,28 @@ class App extends React.Component  {
 		</tr>
 	));
 
-  render () {
-    return (
-      <div className="margen">
-        <table className="tabla">
-          <thead>
-            <tr>
-              <th>
-                Nombre
-              </th>
-              <th>
-                Correo
-              </th>
-              <th>
-                Enlace
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            { this.ponerFilas() }
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+  return (
+    <div className="margen">
+      <table className="tabla">
+        <thead>
+          <tr>
+            <th>
+              Nombre
+            </th>
+            <th>
+              Correo
+            </th>
+            <th>
+              Enlace
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          { ponerFilas() }
+        </tbody>
+      </table>
+    </div>
+  );
 	
 };
 
