79c6ea6188e5e466a6646ae681a0f916ff152b42
Andres Betancurt <andbet050197@gmail.com>
2021-02-19 15:29:48 -0500

    React lifecycle with hoolks

--- blog_platzi/src/componets/App.js
+++ blog_platzi/src/componets/App.js
@@ -1,26 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // React hooks 
 // https://es.reactjs.org/docs/hooks-intro.html
 const App = () => {
 
-  const [usuarios, setUsuarios] = useState([
-    {
-      nombre: 'Rodolfo',
-      correo: 'Rodolfo@platzi.com',
-      enlace: 'Rodolfo.com'
-    },
-    {
-      nombre: 'Antonio',
-      correo: 'Antonio@platzi.com',
-      enlace: 'Antonio.com'
-    },
-    {
-      nombre: 'Ricardo',
-      correo: 'Ricardo@platzi.com',
-      enlace: 'Ricardo.com'
-    },
-  ]);
+  const [usuarios, setUsuarios] = useState([]);
+
+  // useEffect de forma similar a componentDidMount y componentDidUpdate
+  // https://es.reactjs.org/docs/hooks-effect.html
+  useEffect(() => {
+    setUsuarios([
+      {
+        nombre: 'Rodolfo',
+        correo: 'Rodolfo@platzi.com',
+        enlace: 'Rodolfo.com'
+      },
+      {
+        nombre: 'Antonio',
+        correo: 'Antonio@platzi.com',
+        enlace: 'Antonio.com'
+      },
+      {
+        nombre: 'Ricardo',
+        correo: 'Ricardo@platzi.com',
+        enlace: 'Ricardo.com'
+      },
+    ]);
+  });
 
 	const ponerFilas = () => usuarios.map((usuario) => (
 		<tr>
