079a3ea3ce5c3e757ee0547c635a2f537deca993
Andres Betancurt <andbet050197@gmail.com>
2021-02-22 09:45:15 -0500

    Verify promise

--- blog_platzi/src/actions/usersActions.js
+++ blog_platzi/src/actions/usersActions.js
@@ -2,9 +2,14 @@ import axios from 'axios';
 import { TRAER_TODOS } from '../types/usersTypes';
 
 export const traerTodos = () => async (dispatch) => {
-    const { data } = await axios.get('https://jsonplaceholder.typicode.com/users');
-    dispatch({
-        type: TRAER_TODOS,
-        payload: data
-    })
+    try {
+        const { data } = await axios.get('https://jsonplaceholder.typicode.com/users');
+        dispatch({
+            type: TRAER_TODOS,
+            payload: data
+        })
+    }
+    catch (error) {
+        console.log('Error: ', error.message);
+    }
 };
